f89a6a6124d15535abd67843938803f7844180ed
7ayd <86687700+7ayd@users.noreply.github.com>
2021-12-10 02:28:51 -0800

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,30 @@
+// Require dependencies
+import { Client, Intents, ClientUser } from 'discord.js';
+
+const client = new Client({
+    intents:
+        [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]
+});
+
+import { config } from 'dotenv';
+
+config();
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('messageCreate', (message) => {
+    if (message.content === '!price') {
+        message.reply('https://dexscreener.com/avalanche/0x95189f25b4609120f72783e883640216e92732da');
+    }
+
+});
+
+client.on('ready', message => {
+    client.user.setActivity('All praise Thor(This can be edited)', { type: 'STREAMING' });
+
+});
+
+
+client.login(process.env.DISCORD_BOT_TOKEN);
